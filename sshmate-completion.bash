2a93a3e3331a533e8b45e7353e872e6fd50ab003
lrwx00t <lrwx00t@gmail.com>
2023-03-12 21:55:27 -0400

    improve code + provide automcomplete

---
+++ sshmate-completion.bash
@@ -0,0 +1,14 @@
+#!/bin/env bash
+DELIMITER="|"
+SSHMATE_FILE=".sshmate"
+SSHMATE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SSHMATE_FILE_PATH="${SSHMATE_DIR}/${SSHMATE_FILE}"
+_sshmate_completion_function() {
+    connections=()
+    while read -r line; do
+        connections+=("$line")
+    done < <(cut -d "$DELIMITER" -f 1 "${SSHMATE_FILE_PATH}")
+    COMPREPLY=($(compgen -W "${connections[*]}" -- "${COMP_WORDS[COMP_CWORD]}"))
+}
+
+complete -F _sshmate_completion_function sshmate
