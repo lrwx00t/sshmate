23effd429a1d41a434fe56ed047ca6ca79171a2f
lrwx00t <lrwx00t@gmail.com>
2023-03-23 09:53:45 -0400

    update

--- sshmate-completion.bash
+++ sshmate-completion.bash
@@ -1,7 +1,8 @@
 #!/bin/env bash
 DELIMITER="|"
 SSHMATE_FILE=".sshmate"
-SSHMATE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# SSHMATE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SSHMATE_DIR=$(cd "$(dirname "$(readlink "$0")")"; pwd -P)
 SSHMATE_FILE_PATH="${SSHMATE_DIR}/${SSHMATE_FILE}"
 _sshmate_completion_function() {
     connections=()
