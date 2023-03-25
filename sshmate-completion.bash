c697c34e042db683ee581a23098deacb6165d301
lrwx00t <lrwx00t@gmail.com>
2023-03-25 05:50:12 -0400

    fix issues

--- sshmate-completion.bash
+++ sshmate-completion.bash
@@ -2,8 +2,8 @@
 DELIMITER="|"
 SSHMATE_FILE=".sshmate"
 # SSHMATE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-SSHMATE_DIR=$(cd "$(dirname "$(readlink "$0")")"; pwd -P)
-SSHMATE_FILE_PATH="${SSHMATE_DIR}/${SSHMATE_FILE}"
+# SSHMATE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SSHMATE_FILE_PATH=$(sshmate -p)
 _sshmate_completion_function() {
     connections=()
     while read -r line; do
@@ -11,5 +11,4 @@ _sshmate_completion_function() {
     done < <(cut -d "$DELIMITER" -f 1 "${SSHMATE_FILE_PATH}")
     COMPREPLY=($(compgen -W "${connections[*]}" -- "${COMP_WORDS[COMP_CWORD]}"))
 }
-
 complete -F _sshmate_completion_function sshmate
